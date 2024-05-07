f4440a72bda4cce8ff1516c8e1f958eb26da3cf0
alisatresnjic <atresnjic@schueler.hakzell.at>
2024-05-07 09:40:52 +0200

    add weather from hanna

---
+++ README.md
@@ -0,0 +1,2 @@
+PSR-4 autoloading configured. Use "namespace Alisatresnjic\DemoWeather;" in src/
+Include the Composer autoloader with: require 'vendor/autoload.php';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alisatresnjic/demo-weather',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '278d36d105b32a9dbc44c50f6b11bb58b2879f87',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alisatresnjic/demo-weather' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '278d36d105b32a9dbc44c50f6b11bb58b2879f87',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
